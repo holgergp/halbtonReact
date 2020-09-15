7b867c9e93ea1510fb728ffec48861715e3d373f
Holger Grosse-Plankermann <holger.grosse-plankermann@codecentric.de>
2020-09-15 07:50:17 +0200

    Use fade-in fade-out for notes

--- src/frets.js
+++ src/frets.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 export default ({ fret }) => {
   const { note, rootNote } = fret;
-  let noteClass = '';
-  if (note) {
-    noteClass = 'note';
-  }
+  let noteClass = 'note';
   if (rootNote) {
     noteClass += ' root';
+  } else if (!note) {
+    noteClass += ' notDisplayed';
   }
   return (
     <span className={'fret'}>
