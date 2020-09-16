84d5acbfa7efb72c2a47371b393f39cfc3f91ca7
holgergp <holgergp@gmail.com>
2020-09-16 10:28:40 +0000

    Deploying to gh-pages from  @ f591146d2650110b3e3abe01830368a46a65cd90 🚀

---
+++ precache-manifest.b01e4de61b5f71dfed641eff35d800ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5edfb1447c0aca535e314ec930dd0f13",
+    "url": "/halbtonReact/index.html"
+  },
+  {
+    "revision": "8c58ade1b670b3e711f8",
+    "url": "/halbtonReact/static/css/main.2777e850.chunk.css"
+  },
+  {
+    "revision": "7df658166c3de4acde51",
+    "url": "/halbtonReact/static/js/2.9aebc624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/halbtonReact/static/js/2.9aebc624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c58ade1b670b3e711f8",
+    "url": "/halbtonReact/static/js/main.45b1221f.chunk.js"
+  },
+  {
+    "revision": "8e51bd1a978554ffba82",
+    "url": "/halbtonReact/static/js/runtime-main.5ff3a4e5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5795e8e6f08e4e5482b8eadcedda8e0a.js"
+  "/halbtonReact/precache-manifest.b01e4de61b5f71dfed641eff35d800ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/halbtonReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
