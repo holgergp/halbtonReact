b0d3bf8c9cb8ba951625ed69329e9c2384824ca1
Holger Grosse-Plankermann <holger.grosse-plankermann@codecentric.de>
2020-09-20 16:22:01 +0200

    Add basic cypress testing

---
+++ cypress/integration/integration.spec.js
@@ -0,0 +1,20 @@
+
+describe('Tuner works', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+    it('Select Offsetnote', () => {
+      const selectRootNote = '.tuner--rootNote';
+      cy
+        .get(selectRootNote)
+        .select('E');
+      const selectOffset = '.tuner--offset';
+      const offset = '3';
+      cy
+        .get(selectOffset)
+        .type(`{selectall}{backspace}${offset}`);
+
+    const selectOffsetNote = '.display';
+    return cy.get(selectOffsetNote).should('equal', 'G');
+    });
+  });
