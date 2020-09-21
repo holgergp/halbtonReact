269c7d19163002556e30fe7ef42a8e8f57e6bf29
Holger Grosse-Plankermann <holger.grosse-plankermann@codecentric.de>
2020-09-21 21:39:15 +0200

    Add simple fretboard test Prettify cypress

--- cypress/integration/integration.spec.js
+++ cypress/integration/integration.spec.js
@@ -1,19 +1,30 @@
+const selectEWith3Offset = () => {
+  const selectRootNote = '[class*="tuner--rootNote"]';
+  cy.get(selectRootNote).select('E');
+  const selectOffset = '[class*="tuner--offset"]';
+  const offset = '3';
+  cy.get(selectOffset).type(`{selectall}{backspace}${offset}`);
+};
+
 describe('Tuner works', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-    it('Select Offsetnote', () => {
-      const selectRootNote = '[class*="tuner--rootNote"]';
-      cy
-        .get(selectRootNote)
-        .select('E');
-      const selectOffset = '[class*="tuner--offset"]';
-      const offset = '3';
-      cy
-        .get(selectOffset)
-        .type(`{selectall}{backspace}${offset}`);
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('Select Offsetnote', () => {
+    selectEWith3Offset();
 
     const selectOffsetNote = '[class*="display"]';
     return cy.get(selectOffsetNote).should('contain', 'G');
-    });
   });
+});
+
+describe('Fretboard works', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('Select Rootnote and Offsetnote', () => {
+    selectEWith3Offset();
+    cy.get('[class*="fret_note__root"]').should('have.length', 11);
+    cy.get('[class*="fret_note__offset"]').should('have.length', 10);
+  });
+});
